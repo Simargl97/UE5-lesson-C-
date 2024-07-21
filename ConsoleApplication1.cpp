a2b003390ed41cda99b6a2a113c3c279bad502af
sergie <simargll1997@gmail.com>
2024-07-21 23:55:08 +0300

    Task 1

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void print()
+{
+    cout << "Hello World!\n";
+}
+
+//Присваиваем значение переменным и печатаем Hello World!
+
+int main(int argc, char* argv[])
+{
+    int x = 100;
+    3 +7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 1000500;
+
+    int test2 = 1000501;
+
+    int mult = x * y;
+    int random = 1002;
+
+    cout << random;
+}
