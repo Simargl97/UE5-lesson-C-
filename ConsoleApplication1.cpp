5862731d06890e2bdee664e67121d4d046b79f6f
sergie <simargll1997@gmail.com>
2024-07-24 23:01:28 +0300

    Task 14

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -6,4 +6,7 @@ using namespace std;
 int main()
 {
     cout << "the square of their sum: " << sum(10, 10) << "\n";
+    
+    std::string name = "ConsoleApplication";
+    cout << name << "\n" << name.length() << "\n" << name[0] << "\n" << name[name.length() - 1] ;
 }
